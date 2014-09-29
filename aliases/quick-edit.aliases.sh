897c9bdc624486ce1b5251424d412c3e40be48c7
Francis San Juan <francis.sanjuan@gmail.com>
2014-09-29 16:58:19 -0400

    Added new bash_profile and bash-utils aliases to quick-edit aliases file.

--- aliases/quick-edit.aliases.sh
+++ aliases/quick-edit.aliases.sh
@@ -1,6 +1,6 @@
 # quickedit.aliases.sh
 # ver 0.1
-# 2014-01-01
+# 2014-09-29
 # Francis San Juan <francis@ftsanjuan.com>
 #
 # Aliases for quickly editing some important files
@@ -19,5 +19,18 @@ BU_QUICKEDIT_APP="subl"
 # ------------------------------------
 # Bash Profile
 # ------------------------------------
+
 alias bashpro="$BU_QUICKEDIT_APP ~/.bash_profile"
+
+# Reloads .bash_profile
 alias bashrl=". ~/.bash_profile"
+
+# ------------------------------------
+# bash-utils
+# ------------------------------------
+
+# Edit your bash-utils files
+alias bashutils="$BU_QUICKEDIT_APP $BU_ROOT"
+
+# Reloads bash-utils
+alias rbu=". $BU_ROOT/bash-utils.sh"
